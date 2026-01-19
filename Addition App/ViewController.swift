be1dcb16634fc43be2acb1daa4534c57ee2739da
saiva-375 <saivadlamudi375@gmail.com>
2026-01-19 14:12:31 -0500

    Completed the simple task!

--- Addition App/ViewController.swift
+++ Addition App/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var FirstNumber: UITextField!
+    
+    @IBOutlet weak var SecondNumber: UITextField!
+    
+    
+    @IBOutlet weak var ResultSum: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func AddButtonClicked(_ sender: Any) {
+        let firstNumber:Int? = Int(FirstNumber.text!)
+        let secondNumber:Int? = Int(SecondNumber.text!)
+        
+        if let firstNumber = firstNumber, let secondNumber = secondNumber {
+            let sum:Int = firstNumber + secondNumber
+            ResultSum.text = "\(sum)"
+        } else {
+            ResultSum.text = "Please enter valid numbers"
+        }
+    }
+    
 }
 
